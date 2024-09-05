aba0c9614f3336f94c9d388f6a5a5041a06bdc5e
mayank <mayankkeswani11@gmail.com>
2024-09-06 00:49:37 +0530

    correcting bug

--- Backend/routes/contactRoutes.js
+++ Backend/routes/contactRoutes.js
@@ -1,17 +1,26 @@
-const express = require('express');
-const Contact = require('../models/contact');
+const express = require("express");
+const Contact = require("../models/Contact");
 const router = express.Router();
 
 const sendContactDetails = async (req, res) => {
   try {
     const { fullName, email, subject, massage } = req.body;
-    const newContact = await Contact.create({ fullName, email, subject, massage });
-    res.status(200).json({ message: 'Form submitted successfully!', payload: newContact });
+    const newContact = await Contact.create({
+      fullName,
+      email,
+      subject,
+      massage,
+    });
+    res
+      .status(200)
+      .json({ message: "Form submitted successfully!", payload: newContact });
   } catch (error) {
-    res.status(500).json({ message: 'An error occurred. Please try again later.' });
+    res
+      .status(500)
+      .json({ message: "An error occurred. Please try again later." });
   }
-}
+};
 
-router.route('/submit-contact-form').post(sendContactDetails);
+router.route("/submit-contact-form").post(sendContactDetails);
 
 module.exports = router;
